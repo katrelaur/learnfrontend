caddf25096d1abec3d97c68cbe4137564aa0b350
Katre Laur <katrelaur@gmail.com>
2024-01-20 16:35:41 +0200

    Add html plugin and dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     //mode: 'production',
   entry: './src/index.js',
@@ -7,4 +7,12 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  plugins: [new HtmlWebpackPlugin()],
 };
