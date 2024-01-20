f2ef34aa394ea91ac25c7cb330d9c13406a8544c
Katre Laur <katrelaur@gmail.com>
2024-01-20 18:44:06 +0200

    add sass loader and sass

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ const common = require('./common.js');
 common.hello();
 import es from './es.js';
 es.hello();
-import './style.css';
+import './style.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader","sass-loader"],
+      },
     ],
   },
   plugins: [new HtmlWebpackPlugin({
